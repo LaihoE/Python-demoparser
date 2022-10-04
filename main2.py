2307a9c91325dea1adfaedb88da86aa595558631
Laiho <emillaiho@hotmail.fi>
2022-10-05 00:27:57 +0300

    junk

--- main2.py
+++ main2.py
@@ -1,12 +1,17 @@
 import demoparser
 import glob
 
-files = glob.glob("/mnt/c/Users/emill/got/x/*")
+
+files = glob.glob("/home/laiho/Documents/demos/rclonetest/*")
+#files = glob.glob("/mnt/c/Users/emill/got/x/*")
+
+
 okfiles = []
 for file in files:
         if "info" not in file:
             okfiles.append(file)
 
 path = okfiles[0]
+print(path)
 parser = demoparser.DemoParser(path)
 print(parser.parse_events("player_hurt"))

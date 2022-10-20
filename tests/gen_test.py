39fd0acce8745575ba988e424e924869dcef6838
Laiho <emillaiho@hotmail.fi>
2022-10-20 20:15:22 +0300

    early exit ent_ids

--- tests/gen_test.py
+++ tests/gen_test.py
@@ -34,9 +34,14 @@ if __name__ == "__main__":
     import joblib
 
     files = glob.glob("/home/laiho/Documents/demos/faceits/cu/*")[:100]
-    files = ["test.dem"]
+    #files = ["test.dem"]
     with mp.Pool(processes=24) as pool:
         results = list(tqdm.tqdm(pool.imap_unordered(gen_tick_tests, files), total=len(files)))
     print(results)
-    #d = dict(results)
+    
+    d = dict(results)
+    c = joblib.load("sums.pkl")
+    for t in results:
+        print(d[t[0]], c[t[0]])
+
     #joblib.dump(d, "sums.pkl")
